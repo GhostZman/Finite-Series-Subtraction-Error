7d3c14d9f3b57bcb216f2b3e0ffdcd7c5b31a26c
Zachary Ives <zives@hawk.iit.edu>
2024-01-26 15:44:02 -0600

    Initial Commit

---
+++ Finite Series Subtraction Error/Finite_Series_Subtraction_ErrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finite_Series_Subtraction_ErrorApp.swift
+//  Finite Series Subtraction Error
+//
+//  Created by Phys440Zachary on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Finite_Series_Subtraction_ErrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Finite Series Subtraction ErrorUITests/Finite_Series_Subtraction_ErrorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Finite_Series_Subtraction_ErrorUITestsLaunchTests.swift
+//  Finite Series Subtraction ErrorUITests
+//
+//  Created by Phys440Zachary on 1/26/24.
+//
+
+import XCTest
+
+final class Finite_Series_Subtraction_ErrorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
