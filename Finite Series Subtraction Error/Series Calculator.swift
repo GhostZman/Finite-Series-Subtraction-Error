8e7b973f45e45e3055e7507e6ea1f0e0b020ae95
Zachary Ives <zives@hawk.iit.edu>
2024-01-26 16:05:21 -0600

    for real this time

--- Finite Series Subtraction Error/Series Calculator.swift
+++ Finite Series Subtraction Error/Series Calculator.swift
@@ -5,4 +5,26 @@
 //  Created by Phys440Zachary on 1/26/24.
 //
 
-import Foundation
+import SwiftUI
+import Observation
+
+@Observable class FiniteSeries {
+    
+    var series1Result: Float = 0.0
+    var series2Result: Float = 0.0
+    var series3Result: Float = 0.0
+    
+    func computeSeries1(N: Int) -> Float {
+        Task{
+            let returnedResults = await withTaskGroup(
+                of: Float,
+                returning: Float,
+                body:
+            
+            )
+        }
+        
+        
+        return Float(0.0)
+    }
+}
