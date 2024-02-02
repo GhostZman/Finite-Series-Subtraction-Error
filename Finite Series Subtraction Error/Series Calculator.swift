8808ce856c5d46fd41fe2c8640d5a8a80817e069
Zachary Ives <zives@hawk.iit.edu>
2024-02-02 16:25:58 -0600

    Refocus on series 1 using series element calculator class

--- Finite Series Subtraction Error/Series Calculator.swift
+++ Finite Series Subtraction Error/Series Calculator.swift
@@ -25,84 +25,26 @@ import Observation
                 for seriesNIndex in stride(from: 1, through: N, by: 1){
                     
                     taskGroup.addTask{
-                        let result = await pow(-1)
+                        let elementValue = await seriesElement().series1Element(N: seriesNIndex)
+                        
+                        return (seriesNIndex, elementValue)
                     }
                 }
+                var combinedTaskResults :[(Int, Float)] = []
+                for await result in taskGroup {
+                    combinedTaskResults.append(result)
+                }
                 
-            })
-        }
-    }
-
-
-    func computeSeries2(N: Int) -> Float {
-        Task{
-            let firstReturnedResults = await withTaskGroup(
-                of: Float,
-                returning: Float,
-                body: { taskgroup in
-                    for seriesIndex in 1...N {
-                        taskgroup.add{ let seriesNResult: (Float) = (2*seriesIndex - 1)/(2*seriesIndex)
-                            return seriesNResult
-                        }
-                    }
-                    var combinedTaskResults: [Float] = []
-                    for await result in taskgroup{
-                        combinedTaskResults.append(result)
-                    }
-                    return combinedTaskResults
+                return combinedTaskResults
             })
             
-            let secondReturnedResults = await withTaskGroup(
-                of: Float,
-                returning: Float,
-                body: { taskgroup in
-                    for seriesIndex in 1...N {
-                        taskgroup.add{ let seriesNResult: (Float) = (2*seriesIndex)/(2*seriesIndex + 1)
-                            return seriesNResult
-                        }
-                    }
-                    var combinedTaskResults: [Float] = []
-                    for await result in taskgroup{
-                        combinedTaskResults.append(result)
-                    }
-                    return combinedTaskResults
-            })
+            let sortedCombinedResults = combinedResults.sorted(by: { $0.0 < $1.0 })
+            var seriesResult: Float = 0.0
+            for item in sortedCombinedResults{
+                seriesResult = seriesResult + item.1
+            }
+            series1Result = seriesResult
+            return seriesResult
         }
-        let seriesResult: Float = 0.0
-        for result in firstReturnedResults{
-            seriesResult -= result
-        }
-        for result in secondReturnedResults{
-            seriesResult += result
-        }
-        self.series2Result = seriesResult
-        return seriesResult
-    }
-
-func computeSeries3(N: Int) -> Float {
-        Task{
-            let returnedResults = await withTaskGroup(
-                of: Float,
-                returning: Float,
-                body: { taskgroup in
-                    for seriesIndex in 1...N {
-                        taskgroup.add{ let seriesNResult: (Float) = 1/(2*seriesIndex*(2*seriesIndex + 1))
-                            return seriesNResult
-                        }
-                    }
-                    var combinedTaskResults: [Float] = []
-                    for await result in taskgroup{
-                        combinedTaskResults.append(result)
-                    }
-                    return combinedTaskResults
-            })
-        }
-        let seriesResult: Float = 0.0
-        for result in returnedResults{
-            seriesResult += result
-        }
-        
-        self.series3Result = seriesResult
-        return seriesResult
     }
 }
